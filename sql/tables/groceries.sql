f5cc6650813bb64968324ae68192dc49a3106d21
Eric-Carlton <eric121492@gmail.com>
2019-07-19 10:57:40 -0400

    add sql files to create tables

---
+++ sql/tables/groceries.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE IF NOT EXISTS groceries;
+USE groceries;
+CREATE TABLE IF NOT EXISTS groceries (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    unit ENUM('EA', 'LB') NOT NULL,
+    cost_per_unit DECIMAL(6,2) NOT NULL,
+    store_id INT NOT NULL,
+    created_date DATE NOT NULL,
+    CONSTRAINT UNIQUE store_item (name, store_id)
+);
