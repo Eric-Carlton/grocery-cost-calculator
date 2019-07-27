28f9e3a4c4c6184590142d47d25f06581e66253d
Eric-Carlton <eric121492@gmail.com>
2019-07-26 23:39:11 -0400

    add stores_after_delete trigger

---
+++ sql/triggers/stores_after_delete.sql
@@ -0,0 +1,9 @@
+USE groceries;
+
+-- if a store is deleted, delete all associated groceries
+DELIMITER //
+CREATE TRIGGER stores_after_delete AFTER DELETE ON stores FOR EACH ROW
+BEGIN
+	DELETE FROM groceries WHERE store_id = OLD.id;
+END; //
+DELIMITER ;
